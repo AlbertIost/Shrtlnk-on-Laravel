24b7c9549f2b543996e35aa5a64e6aceb38466bb
Albert Iost <albertiost@mail.ru>
2022-10-16 03:05:43 +0300

    View for Main page

---
+++ resources/views/main/show.blade.php
@@ -0,0 +1,15 @@
+<x-layout>
+    <div class="form">
+        <form id="cut-form">
+            <input class="link" id="link" name="link" type="text">
+            <input class="btn " id="send" type="submit" value="Shrt">
+            <input class="btn hidden" id="copy" type="button" value="✂">
+        </form>
+    </div>
+    <div class="error">
+
+    </div>
+    <div class="QR_code-wrapper" id="QR_code-wrapper">
+        <img src="" alt="">
+    </div>
+</x-layout>
