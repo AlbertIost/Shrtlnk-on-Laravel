95d2726aa1f6311a151560f57d970495687e9224
Albert Iost <albertiost@mail.ru>
2022-10-16 17:45:14 +0300

    Create User Seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'login' => Str::random(10),
+            'email' => Str::random(10) . 'mail.ru',
+            'password' => Hash::make(Str::random(8))
+        ]);
+    }
+}
