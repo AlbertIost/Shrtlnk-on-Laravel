ab7a4d3c2505e4da5413f8c9ee4434d42875389f
Albert Iost <albertiost@mail.ru>
2022-10-17 11:48:50 +0300

    Edit for work with database

--- app/Models/Link.php
+++ app/Models/Link.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Link extends Model
 {
+    protected $guarded = [];
+    public $timestamps = false;
     public function user(){
         return $this->belongsTo(User::class);
     }
