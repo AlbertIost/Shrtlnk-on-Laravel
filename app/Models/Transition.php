c02a5558043638431296bcff5155fa521291d453
Albert Iost <albertiost@mail.ru>
2022-10-21 09:22:12 +0300

    Add 'add' method to Transition class

--- app/Models/Transition.php
+++ app/Models/Transition.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -11,5 +12,11 @@ class Transition extends Model
     public $timestamps = false;
     public function link(){
         return $this->belongsTo(Link::class);
+    }
+    public static function Add(Link $link){
+        $transition = new Transition();
+        $transition->link_id = $link->id;
+        $transition->created_on = Carbon::now();
+        $transition->save();
     }
 }
