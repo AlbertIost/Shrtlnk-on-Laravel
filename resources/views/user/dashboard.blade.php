bbabd2fe9017567ef28687cb50ef2477b17cad00
Albert Iost <albertiost@mail.ru>
2022-10-21 09:20:52 +0300

    page for statistics of transitions on user links

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -5,7 +5,7 @@
         <div class="content-wrap container-fluid">
             <div class="row">
                 <div class="col-lg-3 col-md-6 col-6">
-                    <div class="card-statistic ">
+                    <div class="card-statistic block">
                         <div class="icon-wrapper"><x-icon.link/></div>
                         <div class="info">
                             <p class="title">Number of links</p>
@@ -16,7 +16,7 @@
                     </div>
                 </div>
                 <div class="col-lg-3 col-md-6 col-6">
-                    <div class="card-statistic ">
+                    <div class="card-statistic block">
                         <div class="icon-wrapper"><x-icon.link/></div>
                         <div class="info">
                             <p class="title">Transitions today</p>
@@ -25,7 +25,7 @@
                     </div>
                 </div>
                 <div class="col-lg-3 col-md-6 col-6">
-                    <div class="card-statistic ">
+                    <div class="card-statistic block">
                         <div class="icon-wrapper"><x-icon.link/></div>
                         <div class="info">
                             <p class="title">Transitions in a month</p>
@@ -34,7 +34,7 @@
                     </div>
                 </div>
                 <div class="col-lg-3 col-md-6 col-6">
-                    <div class="card-statistic ">
+                    <div class="card-statistic block">
                         <div class="icon-wrapper"><x-icon.statistic/></div>
                         <div class="info">
                             <p class="title">Total transitions</p>
