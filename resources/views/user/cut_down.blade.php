6f006fc3b585e91a6c4f2133a71b8c9496544d62
Albert Iost <albertiost@mail.ru>
2022-10-21 09:17:34 +0300

    page for statistics of transitions on user links

--- resources/views/user/cut_down.blade.php
+++ resources/views/user/cut_down.blade.php
@@ -2,6 +2,42 @@
     <x-profile.sidebar/>
     <div class="main">
         <x-profile.header/>
-        <div class="content-wrap"></div>
+        <div class="content-wrap container-fluid">
+            <form class="links block row">
+                <div class="wrapper-input col-12">
+                    <div class="form-group">
+                        <label class="col-12 control-label">Long link</label>
+                        <input type="text" class="form-control">
+                    </div>
+                </div>
+                <div class="wrapper-input col-12 col-md-6 col-lg 4">
+                    <div class="form-group">
+                        <label class="col-12 control-label">Alias</label>
+                        <input type="text" class="form-control">
+                    </div>
+                </div>
+                <div class="wrapper-input col-12 col-md-6 col-lg 4">
+                    <div class="form-group">
+                        <label class="col-12 control-label">Group</label>
+                        <input type="text" class="form-select">
+                    </div>
+                </div>
+                <div class="wrapper-input col-12 col-md-6 col-lg 4">
+                    <div class="form-group">
+                        <label class="col-12 control-label">Password</label>
+                        <input type="text" class="form-control">
+                    </div>
+                </div>
+                <div class="wrapper-input col-12 col-md-6 col-lg 4">
+                    <div class="form-group">
+                        <label class="col-12 control-label">Active before</label>
+                        <input type="date" class="form-control">
+                    </div>
+                </div>
+                <div class="wrapper-input col-auto d-flex align-items-end">
+                    <input type="submit" value="OK" class="btn btn-primary">
+                </div>
+            </form>
+        </div>
     </div>
 </x-profile.layout>
