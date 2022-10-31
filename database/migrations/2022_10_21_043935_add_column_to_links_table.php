029cb46ce1e57bd7362d7b7c3824237600e30d21
Albert Iost <albertiost@mail.ru>
2022-10-31 13:55:30 +0300

    new migrations

---
+++ database/migrations/2022_10_21_043935_add_column_to_links_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('links', function (Blueprint $table) {
+            $table->foreignId('group_id')
+                ->after('user_id')
+                ->nullable()
+                ->constrained('groups')
+                ->nullOnDelete()
+                ->cascadeOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('links', function (Blueprint $table) {
+            $table->dropForeign(['group_id']);
+            $table->dropColumn('group_id');
+        });
+    }
+};
